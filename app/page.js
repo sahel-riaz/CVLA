dc257a1beb1fbaf082e0e49ba5148ebbd06e7331
sahelriaz <sahelnriaz@gmail.com>
2023-10-31 02:43:03 +0530

    Add: PeopleCard component

--- app/page.js
+++ app/page.js
@@ -1,13 +1,19 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Footer from "@/components/common/Footer";
+import PeopleCard from "@/components/PeopleCard";
 
 export default function Home() {
   return (
-    <div>
-      <div className={styles["footer-container"]}>
-        <Footer />
-      </div>
+    <div className={styles["container"]}>
+      <PeopleCard
+        photo="/svgs/prabhu-sir.png"
+        name="Dr.Prabhu Mohandas"
+        // designation="Associate Professor/CSED<br></br>Associate Dean"
+        contactNumber='9999999999'
+        email='prabhum@nitc.ac.in'
+      />
+      {/* <Footer /> */}
     </div>
   );
 }
---
+++ components/PeopleCard.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./peoplecard.module.css";
+
+export default function PeopleCard({
+  photo,
+  name,
+  designation,
+  contactNumber,
+  email,
+}) {
+  return (
+    <div className={styles["container"]}>
+      <img src={photo} className={styles["photo"]} />
+      <div className={styles["content-container"]}>
+        <div className={styles["name"]}>{name}</div>
+        <div className={styles["designation"]}>
+          Associate Professor/CSED<br></br>Associate Dean
+        </div>
+        <div className={styles["contact-wrapper"]}>
+          <div className={styles["contact"]}>
+            <img src="/svgs/call.svg" className={styles["image"]} />
+            <p>{contactNumber}</p>
+          </div>
+          <div className={styles["contact"]}>
+            <img src="/svgs/mail.svg" className={styles["image"]} />
+            <p>{email}</p>
+          </div>
+        </div>
+        <div className={styles["college"]}>
+          National Institute of Technology, Calicut<br></br>Kerala, 673601
+        </div>
+      </div>
+    </div>
+  );
+}
