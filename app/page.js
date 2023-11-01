fdd7cfc328a88b8be46a666c5502a91488d146b7
sahelriaz <sahelnriaz@gmail.com>
2023-11-01 12:39:09 +0530

    Fix: small footer changes

--- app/page.js
+++ app/page.js
@@ -6,14 +6,8 @@ import PeopleCard from "@/components/PeopleCard";
 export default function Home() {
   return (
     <div className={styles["container"]}>
-      <PeopleCard
-        photo="/svgs/prabhu-sir.png"
-        name="Dr.Prabhu Mohandas"
-        // designation="Associate Professor/CSED<br></br>Associate Dean"
-        contactNumber='9999999999'
-        email='prabhum@nitc.ac.in'
-      />
-      {/* <Footer /> */}
+      
+      <Footer />
     </div>
   );
 }
