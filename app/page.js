8a4586ecd115a20f8cadae6ae82352fb38794164
sahelriaz <sahelnriaz@gmail.com>
2023-11-01 12:48:26 +0530

    Add: Navbar layout done

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Navbar from "@/components/common/Navbar";
 import Footer from "@/components/common/Footer";
 import PeopleCard from "@/components/PeopleCard";
 
 export default function Home() {
   return (
     <div className={styles["container"]}>
-      
+      <Navbar />
+
       <Footer />
     </div>
   );
---
+++ components/common/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <div className={styles["container"]}>
+      <img src="/svgs/logo.svg" className={styles["logo"]} />
+      <div className={styles['button-container']}>
+        <div className={styles['button']}>HOME</div>
+        <div className={styles['button']}>RESEARCH</div>
+        <div className={styles['button']}>PUBLICATIONS</div>
+        <div className={styles['button']}>PROJECTS</div>
+        <div className={styles['button']}>STUDENTS</div>
+        <div className={styles['button']}>MEMBERS</div>
+        <div className={styles['button']}>CONTACT US</div>
+      </div>
+    </div>
+  );
+}
