c99a5729a5b5dbecd124105d2917618b2690f9c3
sahelriaz <sahelnriaz@gmail.com>
2023-11-02 15:24:53 +0530

    Update: Research page small changes

--- app/research/page.js
+++ app/research/page.js
@@ -6,46 +6,48 @@ import Footer from "@/components/common/Footer";
 
 export default function page() {
   return (
-    <div className={styles["container"]}>
-      <Navbar />
-      <div className={styles["research-container"]}>
-        <div className={styles["heading"]}>Research</div>
-        <div className={styles["research-cards"]}>
-          <ResearchCard
-            author="Dr. Ashish Bamania"
-            startDate="23 September 2023"
-            endDate="Present"
-            title="Histogram Layers for Improved Target Classification"
-            sponsor="Massachusetts Institute of Technology Lincoln Laboratory"
-            image="/svgs/blank-image.svg"
-          />
-          <ResearchCard
-            author="Dr. Ashish Bamania"
-            startDate="23 September 2023"
-            endDate="Present"
-            title="Histogram Layers for Improved Target Classification"
-            sponsor="Massachusetts Institute of Technology Lincoln Laboratory"
-            image="/svgs/blank-image.svg"
-          />
-          <ResearchCard
-            author="Dr. Ashish Bamania"
-            startDate="23 September 2023"
-            endDate="Present"
-            title="Histogram Layers for Improved Target Classification"
-            sponsor="Massachusetts Institute of Technology Lincoln Laboratory"
-            image="/svgs/blank-image.svg"
-          />
-          <ResearchCard
-            author="Dr. Ashish Bamania"
-            startDate="23 September 2023"
-            endDate="Present"
-            title="Histogram Layers for Improved Target Classification"
-            sponsor="Massachusetts Institute of Technology Lincoln Laboratory"
-            image="/svgs/blank-image.svg"
-          />
+    <div className={styles["big-container"]}>
+      {/* <Navbar /> */}
+      <div className={styles["container"]}>
+        <div className={styles["research-container"]}>
+          <div className={styles["heading"]}>Research</div>
+          <div className={styles["research-cards"]}>
+            <ResearchCard
+              author="Dr. Ashish Bamania"
+              startDate="23 September 2023"
+              endDate="Present"
+              title="Histogram Layers for Improved Target Classification"
+              sponsor="Massachusetts Institute of Technology Lincoln Laboratory"
+              image="/svgs/blank-image.svg"
+            />
+            <ResearchCard
+              author="Dr. Ashish Bamania"
+              startDate="23 September 2023"
+              endDate="Present"
+              title="Histogram Layers for Improved Target Classification"
+              sponsor="Massachusetts Institute of Technology Lincoln Laboratory"
+              image="/svgs/blank-image.svg"
+            />
+            <ResearchCard
+              author="Dr. Ashish Bamania"
+              startDate="23 September 2023"
+              endDate="Present"
+              title="Histogram Layers for Improved Target Classification"
+              sponsor="Massachusetts Institute of Technology Lincoln Laboratory"
+              image="/svgs/blank-image.svg"
+            />
+            <ResearchCard
+              author="Dr. Ashish Bamania"
+              startDate="23 September 2023"
+              endDate="Present"
+              title="Histogram Layers for Improved Target Classification"
+              sponsor="Massachusetts Institute of Technology Lincoln Laboratory"
+              image="/svgs/blank-image.svg"
+            />
+          </div>
         </div>
       </div>
-      <Footer />  
+      <Footer />
     </div>
   );
 }
