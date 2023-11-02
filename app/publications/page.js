383b354b982ec0aebfbe1432a300d497c56defe9
sahelriaz <sahelnriaz@gmail.com>
2023-11-03 01:26:30 +0530

    Add: Publications page layout done

--- app/publications/page.js
+++ app/publications/page.js
@@ -3,49 +3,125 @@ import styles from "./publications.module.css";
 import PublicationsCard from "@/components/PublicationsCard";
 import Navbar from "@/components/common/Navbar";
 import QuickNavigation from "@/components/QuickNavigation";
+import Footer from "@/components/common/Footer";
 
 export default function Publications() {
   const quickLinks = {
-    "Journals": "Explore the latest research in academic journals",
-    "Conferences": "Discover insights from conferences and symposiums",
-    "Patents": "Access patented innovations and intellectual property",
-    "Book chapters": "Find in-depth knowledge in published book chapters"
+    Journals: "Explore the latest research in academic journals",
+    Conferences: "Discover insights from conferences and symposiums",
+    Patents: "Access patented innovations and intellectual property",
+    "Book chapters": "Find in-depth knowledge in published book chapters",
   };
   return (
     <div className={styles["container"]}>
-      {/* <Navbar /> */}
+      <Navbar />
       <div className={styles["main-container"]}>
-        <QuickNavigation quickLinks={quickLinks} />
-        {/* <div className={styles["publication-cards"]}>
-          <PublicationsCard
-            image="/svgs/blank-image.svg"
-            date="23 September 2023"
-            title="Multi‑modal, Multi‑task Data Analysis for Automated Plant Phenotyping"
-            references='J. Ritu, E. Barnes, R. Martell, A. Dine and J. Peeples, "Histogram
+        <div className={styles["left-container"]}>
+          <div className={styles["publications"]}>
+            <div className={styles["title"]}>Publications</div>
+            <div className={styles["publications-wrapper"]}>
+              <div className={styles["publication-title"]}>Journals</div>
+              <div className={styles["publication-list"]}>
+                <PublicationsCard
+                  image="/svgs/blank-image.svg"
+                  date="23 September 2023"
+                  title="Multi‑modal, Multi‑task Data Analysis for Automated Plant Phenotyping"
+                  references='J. Ritu, E. Barnes, R. Martell, A. Dine and J. Peeples, "Histogram
           Layer Time Delay Neural Network For Passive Sonar Classification,"
           2023 IEEE Workshop on Applications of Signal Processing to Audio and
           Acoustics (WASPAA). doi: 10.1109/WASPAA58266.2023.10248102.'
-          />
-          <PublicationsCard
-            image="/svgs/blank-image.svg"
-            date="23 September 2023"
-            title="Multi‑modal, Multi‑task Data Analysis for Automated Plant Phenotyping"
-            references='J. Ritu, E. Barnes, R. Martell, A. Dine and J. Peeples, "Histogram
+                />
+                <PublicationsCard
+                  image="/svgs/blank-image.svg"
+                  date="23 September 2023"
+                  title="Multi‑modal, Multi‑task Data Analysis for Automated Plant Phenotyping"
+                  references='J. Ritu, E. Barnes, R. Martell, A. Dine and J. Peeples, "Histogram
           Layer Time Delay Neural Network For Passive Sonar Classification,"
           2023 IEEE Workshop on Applications of Signal Processing to Audio and
           Acoustics (WASPAA). doi: 10.1109/WASPAA58266.2023.10248102.'
-          />
-          <PublicationsCard
-            image="/svgs/blank-image.svg"
-            date="23 September 2023"
-            title="Multi‑modal, Multi‑task Data Analysis for Automated Plant Phenotyping"
-            references='J. Ritu, E. Barnes, R. Martell, A. Dine and J. Peeples, "Histogram
+                />
+              </div>
+            </div>
+            <div className={styles["publications-wrapper"]}>
+              <div className={styles["publication-title"]}>Conferences</div>
+              <div className={styles["publication-list"]}>
+                <PublicationsCard
+                  image="/svgs/blank-image.svg"
+                  date="23 September 2023"
+                  title="Multi‑modal, Multi‑task Data Analysis for Automated Plant Phenotyping"
+                  references='J. Ritu, E. Barnes, R. Martell, A. Dine and J. Peeples, "Histogram
           Layer Time Delay Neural Network For Passive Sonar Classification,"
           2023 IEEE Workshop on Applications of Signal Processing to Audio and
           Acoustics (WASPAA). doi: 10.1109/WASPAA58266.2023.10248102.'
-          />
-        </div> */}
+                />
+                <PublicationsCard
+                  image="/svgs/blank-image.svg"
+                  date="23 September 2023"
+                  title="Multi‑modal, Multi‑task Data Analysis for Automated Plant Phenotyping"
+                  references='J. Ritu, E. Barnes, R. Martell, A. Dine and J. Peeples, "Histogram
+          Layer Time Delay Neural Network For Passive Sonar Classification,"
+          2023 IEEE Workshop on Applications of Signal Processing to Audio and
+          Acoustics (WASPAA). doi: 10.1109/WASPAA58266.2023.10248102.'
+                />
+              </div>
+            </div>
+            <div className={styles["publications-wrapper"]}>
+              <div className={styles["publication-title"]}>Patents</div>
+              <div className={styles["publication-list"]}>
+                <PublicationsCard
+                  image="/svgs/blank-image.svg"
+                  date="23 September 2023"
+                  title="Multi‑modal, Multi‑task Data Analysis for Automated Plant Phenotyping"
+                  references='J. Ritu, E. Barnes, R. Martell, A. Dine and J. Peeples, "Histogram
+          Layer Time Delay Neural Network For Passive Sonar Classification,"
+          2023 IEEE Workshop on Applications of Signal Processing to Audio and
+          Acoustics (WASPAA). doi: 10.1109/WASPAA58266.2023.10248102.'
+                />
+                <PublicationsCard
+                  image="/svgs/blank-image.svg"
+                  date="23 September 2023"
+                  title="Multi‑modal, Multi‑task Data Analysis for Automated Plant Phenotyping"
+                  references='J. Ritu, E. Barnes, R. Martell, A. Dine and J. Peeples, "Histogram
+          Layer Time Delay Neural Network For Passive Sonar Classification,"
+          2023 IEEE Workshop on Applications of Signal Processing to Audio and
+          Acoustics (WASPAA). doi: 10.1109/WASPAA58266.2023.10248102.'
+                />
+              </div>
+            </div>
+            <div className={styles["publications-wrapper"]}>
+              <div className={styles["publication-title"]}>Book Chapters</div>
+              <div className={styles["publication-list"]}>
+                <PublicationsCard
+                  image="/svgs/blank-image.svg"
+                  date="23 September 2023"
+                  title="Multi‑modal, Multi‑task Data Analysis for Automated Plant Phenotyping"
+                  references='J. Ritu, E. Barnes, R. Martell, A. Dine and J. Peeples, "Histogram
+          Layer Time Delay Neural Network For Passive Sonar Classification,"
+          2023 IEEE Workshop on Applications of Signal Processing to Audio and
+          Acoustics (WASPAA). doi: 10.1109/WASPAA58266.2023.10248102.'
+                />
+                <PublicationsCard
+                  image="/svgs/blank-image.svg"
+                  date="23 September 2023"
+                  title="Multi‑modal, Multi‑task Data Analysis for Automated Plant Phenotyping"
+                  references='J. Ritu, E. Barnes, R. Martell, A. Dine and J. Peeples, "Histogram
+          Layer Time Delay Neural Network For Passive Sonar Classification,"
+          2023 IEEE Workshop on Applications of Signal Processing to Audio and
+          Acoustics (WASPAA). doi: 10.1109/WASPAA58266.2023.10248102.'
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles["right-container"]}>
+          <div className={styles["quick-nav-container"]}>
+            <QuickNavigation quickLinks={quickLinks} />
+          </div>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 }
+
+//  <QuickNavigation quickLinks={quickLinks} />
