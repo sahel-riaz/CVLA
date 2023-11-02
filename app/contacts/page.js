e50a8c4501c2054756d52889b15b1c09320aba08
sahelriaz <sahelnriaz@gmail.com>
2023-11-02 10:17:56 +0530

    Update: Contact us page done

--- app/contacts/page.js
+++ app/contacts/page.js
@@ -1,25 +1,34 @@
 import React from "react";
 import styles from "./contacts.module.css";
+import Navbar from "@/components/common/Navbar";
+import Footer from "@/components/common/Footer";
 import ContactUs from "@/components/ContactUs";
 
 export default function Contacts() {
   return (
     <div className={styles["container"]}>
-      <ContactUs
-        image="/svgs/contact-call.svg"
-        title="BY PHONE"
-        info="+91 9999999999"
-      />
-      <ContactUs
-        image="/svgs/contact-loc.svg"
-        title="FIND US"
-        info="National Institute of Technology Calicut, Kerala 673601"
-      />
-      <ContactUs
-        image="/svgs/contact-email.svg"
-        title="BY EMAIL"
-        info="contactus@nitc.ac.in"
-      />
+      <Navbar />
+      <div className={styles["content-container"]}>
+        <div className={styles["heading"]}>Contact Us</div>
+        <div className={styles["cards-container"]}>
+          <ContactUs
+            image="/svgs/contact-call.svg"
+            title="BY PHONE"
+            info="+91 9999999999"
+          />
+          <ContactUs
+            image="/svgs/contact-loc.svg"
+            title="FIND US"
+            info="National Institute of Technology Calicut, Kerala 673601"
+          />
+          <ContactUs
+            image="/svgs/contact-email.svg"
+            title="BY EMAIL"
+            info="contactus@nitc.ac.in"
+          />
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
