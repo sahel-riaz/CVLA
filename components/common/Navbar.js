2c94f75d2f0f356e8a4b452585db5816d0d17217
Aadhavan <aadhavanlenin@gmail.com>
2023-12-21 12:02:11 +0300

    Fix: navbar width

--- components/common/Navbar.js
+++ components/common/Navbar.js
@@ -1,72 +1,45 @@
-"use client";
+'use client'
 
-import React from "react";
-import { useState } from "react";
-import styles from "./navbar.module.css";
-import { useRouter } from "next/navigation";
+import React from 'react'
+import { useState } from 'react'
+import styles from './navbar.module.css'
+import { useRouter } from 'next/navigation'
 
 export default function Navbar() {
-  const router = useRouter();
-  const [nav, setNav] = useState(false);
+	const router = useRouter()
+	const [nav, setNav] = useState(false)
 
-  function handleNav() {
-    setNav(!nav);
-  }
+	function handleNav() {
+		setNav(!nav)
+	}
 
-  return (
-    <div className={styles["container"]}>
-      <img
-        src="/svgs/logo.svg"
-        className={styles["logo"]}
-        onClick={() => router.push("/")}
-      />
-      <div className={styles["button-container"]}>
-        <img
-          src="/svgs/hamburger.svg"
-          className={styles["hamburger-icon"]}
-          onClick={handleNav}
-        />
-        <div
-          className={`${styles["button-dropdown"]} ${
-            nav ? styles["show"] : ""
-          }`}
-        >
-          <div className={styles["button"]} onClick={() => router.push("/")}>
-            HOME
-          </div>
-          <div
-            className={styles["button"]}
-            onClick={() => router.push("/research")}
-          >
-            RESEARCH
-          </div>
-          <div
-            className={styles["button"]}
-            onClick={() => router.push("/publications")}
-          >
-            PUBLICATIONS
-          </div>
-          <div
-            className={styles["button"]}
-            onClick={() => router.push("/projects")}
-          >
-            PROJECTS
-          </div>
-          <div
-            className={styles["button"]}
-            onClick={() => router.push("/people")}
-          >
-            PEOPLE
-          </div>
-          {/* <div className={styles["button"]}>EVENTS</div> */}
-          <div
-            className={styles["button"]}
-            onClick={() => router.push("/contacts")}
-          >
-            CONTACT US
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles['container']}>
+			<img src='/svgs/logo.svg' className={styles['logo']} onClick={() => router.push('/')} />
+			<div className={styles['button-container']}>
+				<img src='/svgs/hamburger.svg' className={styles['hamburger-icon']} onClick={handleNav} />
+				<div className={`${styles['button-dropdown']} ${nav ? styles['show'] : ''}`}>
+					<div className={styles['button']} onClick={() => router.push('/')}>
+						HOME
+					</div>
+					<div className={styles['button']} onClick={() => router.push('/research')}>
+						RESEARCH
+					</div>
+					<div className={styles['button']} onClick={() => router.push('/publications')}>
+						PUBLICATIONS
+					</div>
+					<div className={styles['button']} onClick={() => router.push('/projects')}>
+						PROJECTS
+					</div>
+					<div className={styles['button']} onClick={() => router.push('/people')}>
+						PEOPLE
+					</div>
+					{/* <div className={styles["button"]}>EVENTS</div> */}
+					<div className={styles['button']} onClick={() => router.push('/contacts')}>
+						CONTACT US
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
