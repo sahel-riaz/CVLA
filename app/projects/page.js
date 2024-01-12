3e558bc7ccf1bb1390cc6814374082d1d0a558a1
Aadhavan <aadhavanlenin@gmail.com>
2024-01-12 10:57:06 +0530

    Add: projects pocketbase

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,89 +1,104 @@
-"use client";
+'use client'
 
-import React from "react";
-import styles from "./projects.module.css";
-import { useRef } from "react";
-import Navbar from "@/components/common/Navbar";
-import Footer from "@/components/common/Footer";
-import QuickNavigation from "@/components/QuickNavigation";
-import ProjectCard from "@/components/ProjectCard";
-import { pb } from "@/lib/pocketbase";
+import React, { useEffect, useState } from 'react'
+import styles from './projects.module.css'
+import { useRef } from 'react'
+import Navbar from '@/components/common/Navbar'
+import Footer from '@/components/common/Footer'
+import QuickNavigation from '@/components/QuickNavigation'
+import ProjectCard from '@/components/ProjectCard'
+import { pb } from '@/lib/pocketbase'
+import Link from 'next/link'
 
 export default function Projects() {
-  const ongoingRef = useRef(null);
-  const completedRef = useRef(null);
+	const ongoingRef = useRef(null)
+	const completedRef = useRef(null)
 
-  const quickLinks = {
-    "Ongoing projects": "Explore Ongoing Projects for current initiatives",
-    "Completed projects": "Review Completed Projects for past achievements",
-  };
+	const [ongoingProjects, setOngoingProjects] = useState([])
+	const [completedProjects, setCompletedProjects] = useState([])
 
-  return (
-    <div className={styles["container"]}>
-      <Navbar />
-      <div className={styles["main-container"]}>
-        <div className={styles["left-container"]}>
-          <div className={styles["projects"]}>
-            <div className={styles["title"]}>Projects</div>
-            <div className={styles["projects-wrapper"]} ref={ongoingRef}>
-              <div className={styles["project-title"]}>Ongoing projects</div>
-              <div className={styles["project-list"]}>
-                <ProjectCard
-                  image="/svgs/blank-image.svg"
-                  date="2023-2026"
-                  title="[P07] DST-SERB"
-                  desc="A Step towards Smart City Environment: A Reinforcement Learning Based
-                  Intelligent Traffic control for Transportation System for Smart Cities in Kerala"
-                  sponsor="Science and Engineering Research Board, Department of Science and Technology, Government of India"
-                  amount="Rs. 25,40,000/-"
-                />
-                <ProjectCard
-                  image="/svgs/blank-image.svg"
-                  date="2023-2026"
-                  title="[P07] DST-SERB"
-                  desc="A Step towards Smart City Environment: A Reinforcement Learning Based
-                  Intelligent Traffic control for Transportation System for Smart Cities in Kerala"
-                  sponsor="Science and Engineering Research Board, Department of Science and Technology, Government of India"
-                  amount="Rs. 25,40,000/-"
-                />
-              </div>
-            </div>
-            <div className={styles["projects-wrapper"]} ref={completedRef}>
-              <div className={styles["project-title"]}>Completed projects</div>
-              <div className={styles["project-list"]}>
-                <ProjectCard
-                  image="/svgs/blank-image.svg"
-                  date="2023-2026"
-                  title="[P07] DST-SERB"
-                  desc="A Step towards Smart City Environment: A Reinforcement Learning Based
-                  Intelligent Traffic control for Transportation System for Smart Cities in Kerala"
-                  sponsor="Science and Engineering Research Board, Department of Science and Technology, Government of India"
-                  amount="Rs. 25,40,000/-"
-                />
-                <ProjectCard
-                  image="/svgs/blank-image.svg"
-                  date="2023-2026"
-                  title="[P07] DST-SERB"
-                  desc="A Step towards Smart City Environment: A Reinforcement Learning Based
-                  Intelligent Traffic control for Transportation System for Smart Cities in Kerala"
-                  sponsor="Science and Engineering Research Board, Department of Science and Technology, Government of India"
-                  amount="Rs. 25,40,000/-"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className={styles["right-container"]}>
-          <div className={styles["quick-nav-container"]}>
-            <QuickNavigation
-              quickLinks={quickLinks}
-              ongoingRef={ongoingRef}
-              completedRef={completedRef}
-            />
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </div>
-  );
+	const quickLinks = {
+		'Ongoing projects': 'Explore Ongoing Projects for current initiatives',
+		'Completed projects': 'Review Completed Projects for past achievements',
+	}
+
+	useEffect(() => {
+		;(async () => {
+			const projectRecords = await pb.collection('projects').getFullList({
+				$autoCancel: false,
+			})
+
+			const tempOngoingProjects = []
+			const tempCompletedProjects = []
+
+			for (let r of projectRecords) {
+				r.thumbnail = pb.getFileUrl(r, r.thumbnail)
+				if (r.type == 'ongoing') tempOngoingProjects.push(r)
+				else if (r.type == 'completed') tempCompletedProjects.push(r)
+			}
+			setOngoingProjects(tempOngoingProjects)
+			setCompletedProjects(tempCompletedProjects)
+		})()
+	})
+
+	return (
+		<div className={styles['container']}>
+			<Navbar />
+			<div className={styles['main-container']}>
+				<div className={styles['left-container']}>
+					<div className={styles['projects']}>
+						<div className={styles['title']}>Projects</div>
+						{ongoingProjects?.length ? (
+							<div className={styles['projects-wrapper']} ref={ongoingRef}>
+								<div className={styles['project-title']}>Ongoing projects</div>
+								<div className={styles['project-list']}>
+									{ongoingProjects?.map((project, index) => (
+										<Link href={`/projects/${project?.id}`} key={index}>
+											<ProjectCard
+												image={project?.thumbnail}
+												date={project?.start + '-' + project?.end}
+												title={project?.title}
+												desc={project?.description}
+												sponsor={project?.sponsor}
+												amount={project?.amount}
+											/>
+										</Link>
+									))}
+								</div>
+							</div>
+						) : null}
+						{completedProjects?.length ? (
+							<div className={styles['projects-wrapper']} ref={completedRef}>
+								<div className={styles['project-title']}>Completed projects</div>
+								<div className={styles['project-list']}>
+									{completedProjects?.map((project, index) => (
+										<Link href={`/projects/${project?.id}`} key={index}>
+											<ProjectCard
+												image={project?.thumbnail}
+												date={project?.start + ' - ' + project?.end}
+												title={project?.title}
+												desc={project?.description}
+												sponsor={project?.sponsor}
+												amount={project?.amount}
+											/>
+										</Link>
+									))}
+								</div>
+							</div>
+						) : null}
+					</div>
+				</div>
+				<div className={styles['right-container']}>
+					<div className={styles['quick-nav-container']}>
+						<QuickNavigation
+							quickLinks={quickLinks}
+							ongoingRef={ongoingRef}
+							completedRef={completedRef}
+						/>
+					</div>
+				</div>
+			</div>
+			<Footer />
+		</div>
+	)
 }
